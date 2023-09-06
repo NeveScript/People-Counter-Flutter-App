85c1935538889392192440ccdbcd6bd68721a52e
NekoYasha <autornekoyasha@gmail.com>
2023-09-06 13:41:48 -0300

    Create Home Page Widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.yellow,
-        alignment: Alignment.center,
-        child: const Text("Hello World!"),
-      ),
-    );
-  }
+    return const MaterialApp(
+      home: const HomePage(),
+    ); // MaterialApp
+  } // build
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Can Enter",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800)), // TextStyle
+          Text("0",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800)), // TextStyle
+        ], // children
+      ), // Column
+    ); // Sacaffold
+
+  } // build
 }
