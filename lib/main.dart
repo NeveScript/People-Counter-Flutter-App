5fafc65d02316ddf0dab65e9fa099af486dad088
NekoYasha <autornekoyasha@gmail.com>
2023-09-08 11:50:55 -0300

    Add button and padding to the HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -39,39 +39,66 @@ class HomePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("Can Enter",
+          const Text(
+            "Can Enter",
+            style: TextStyle(
+                fontSize: 30, color: Colors.white, fontWeight: FontWeight.w800),
+          ), // TextStyle
+          const Padding(
+            padding: EdgeInsets.all(32),
+            child: Text(
+              "0",
               style: TextStyle(
                   fontSize: 30,
                   color: Colors.white,
-                  fontWeight: FontWeight.w800)), // TextStyle
-          const Text("0",
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w800)), // TextStyle
+                  fontWeight: FontWeight.w800),
+            ), // TextStyle
+          ), // Padding
+          const SizedBox(width: 32,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextButton(
                 onPressed: decrement,
-                child: const Text("Left",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                    )), //Text
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ), // RoundedRectangleBorder
+                ), // TextButton.styleFrom
+                child: const Text(
+                  "Left",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                  ),
+                ), //Text
               ), // TextButton
+
               TextButton(
                 onPressed: increment,
-                child: const Text("Left",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                    )), //Text
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ), // RoundedRectangleBorder
+                ), // TextButton.styleFrom
+                child: const Text(
+                  "Entered",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                  ),
+                ), //Text
               ), // TextButton
             ], // children
           ), // Row
         ], // children
       ), // Column
-    ); // Sacaffold
+    ); // Scaffold
   } // build
 }
